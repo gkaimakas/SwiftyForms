37ff3a058b1b05e74ad543dc5c27045ccc1808b6
gkaimakas <gkaimakas@gmail.com>
2016-06-29 11:46:35 +0300

    added dateFormat on DateInput

--- SwiftyForms.podspec
+++ SwiftyForms.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
 	s.name             = 'SwiftyForms'
-	s.version          = '0.3.2'
+	s.version          = '0.3.3'
 	s.summary          = 'Swifty way to create forms in iOS'
 
 	s.description      = <<-DESC
--- SwiftyForms/Classes/Inputs/DateInput.swift
+++ SwiftyForms/Classes/Inputs/DateInput.swift
@@ -10,5 +10,13 @@ import Foundation
 import SwiftValidators
 
 public class DateInput: Input {
+	public var dateFormat: String = "dd-MM-yyyy"
+	
+	/// Sets the format of the displayed date
+	
+	public func setDateFormat(format: String) -> DateInput {
+		dateFormat = format
+		return self
+	}
 	
 }
